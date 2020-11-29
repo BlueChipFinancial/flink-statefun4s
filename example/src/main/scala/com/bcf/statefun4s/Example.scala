2546e5b94ca7e0b7ddbea351b35c8f265f29b8d1
Timothy Bess <tdbgamer@gmail.com>
2020-11-29 03:25:58 -0500

    add egress to example

--- example/src/main/scala/com/bcf/statefun4s/Example.scala
+++ example/src/main/scala/com/bcf/statefun4s/Example.scala
@@ -1,11 +1,11 @@
 package com.bcf.statefun4s
 
-import scala.annotation.nowarn
 import scala.concurrent.ExecutionContext
 
 import cats.effect._
 import cats.implicits._
 import com.bcf.statefun4s.example.Example._
+import org.apache.flink.statefun.flink.io.generated.Kafka.KafkaProducerRecord
 import org.http4s.server.blaze.BlazeServerBuilder
 
 import StatefulFunction._
@@ -17,12 +17,21 @@ object Example extends IOApp {
     StatefulFunction[F, Unit].sendMsg("example", "greeter", input.name, input)
 
   def greeter[F[_]: StatefulFunction[*[_], GreeterState]: Sync](
-      @nowarn input: GreeterRequest
+      input: GreeterRequest
   ): F[Unit] = {
     val statefun = StatefulFunction[F, GreeterState]
     for {
       newCount <- statefun.insideCtx(_.num + 1)
       _ <- statefun.modifyCtx(_.copy(newCount))
+      _ <- statefun.sendEgressMsg(
+        "greeting",
+        "greets",
+        KafkaProducerRecord(
+          input.name,
+          GreeterResponse(s"Saw ${input.name} ${newCount} time(s)").toByteString,
+          "greets"
+        )
+      )
       _ <- Sync[F].delay(println(s"Saw $newCount time(s)"))
     } yield ()
   }
