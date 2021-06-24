cc277aa1fdc29db2a7452650936179761e3c1aed
Timothy Bess <tdbgamer@gmail.com>
2021-06-24 17:05:28 -0400

    segregate type/untyped APIs

--- macros/src/main/scala/com/bcf/statefun4s/generic/FlinkFunction.scala
+++ macros/src/main/scala/com/bcf/statefun4s/generic/FlinkFunction.scala
@@ -4,17 +4,17 @@ import scala.annotation.{StaticAnnotation, nowarn}
 import scala.reflect.macros.whitebox
 
 import cats.implicits._
-import cats.mtl.Raise
-import com.bcf.statefun4s.{FlinkError, FunctionDescriptor, StatefulFunction}
+import cats.mtl.Handle
+import com.bcf.statefun4s.{Codec, FlinkError, FunctionDescriptor, StatefulFunction}
 
 @nowarn("msg=never used")
 class FlinkFunction(namespace: String, `type`: String) extends StaticAnnotation {
   def macroTransform(annottees: Any*): Any = macro FlinkFunctionImpl.impl
 }
 
+@nowarn("msg=never used")
+class MapInputs(func: (Any => Any)*) extends StaticAnnotation
 class FlinkMsg extends StaticAnnotation
-class ProtoInput extends StaticAnnotation
-class CodecInput extends StaticAnnotation
 
 @nowarn("msg=never used")
 object FlinkFunctionImpl {
@@ -27,38 +27,48 @@ object FlinkFunctionImpl {
         c.abort(c.enclosingPosition, "FlinkFunction arguments didn't match what macro extracts")
     }
     val input = annottees.head
-    val addition = input match {
+    val out = input match {
       case func: DefDef =>
         val inputVar =
-          func.vparamss
-            .collectFirst(_.collectFirst {
+          func.vparamss.flatten
+            .collectFirst {
               case field @ ValDef(Modifiers(_, _, annotations), _, _, _)
                   if annotations.exists(_ equalsStructure q"new FlinkMsg()") =>
                 field
-            })
-            .flatten
+            }
+
         val higherKindedType =
           func.vparamss.flatten
+            .map(_.tpt)
             .collectFirst {
-              case q"$_ val $_: StatefulFunction[$_, $_][$higherKindedType] = $_" =>
-                higherKindedType.asInstanceOf[Ident]
-              case q"$_ val $_: StatefulFunction[$higherKindedType, $_] = $_" =>
-                higherKindedType.asInstanceOf[Ident]
-              case q"$_ val $_: $_[$higherKindedType] = $_" =>
-                higherKindedType.asInstanceOf[Ident]
+              case tq"TypedStatefulFunction[$higherKindedType, $_, $returnType]" =>
+                (higherKindedType.asInstanceOf[Ident], returnType.some)
+              case tq"$_#$_[$higherKindedType]" =>
+                (higherKindedType.asInstanceOf[Ident], none)
+              case tq"StatefulFunction[$higherKindedType, $_]" =>
+                (higherKindedType.asInstanceOf[Ident], none)
             }
+
+        val inputMappings =
+          func.vparamss.flatten
+            .collectFirst {
+              case field @ ValDef(Modifiers(_, _, annotations), _, _, _)
+                  if annotations.exists(_ equalsStructure q"new FlinkMsg()") =>
+                field
+            }
+
+        val mappings = func.mods.annotations.collect {
+          case q"new MapInputs(..$funcs)" => funcs.map(_.asInstanceOf[Function])
+        }.flatten
 
         (inputVar product higherKindedType)
           .map {
-            case (input, higherKindedType) =>
+            case (input, (higherKindedType, returnType)) =>
               val flinkError = tq"${symbolOf[FlinkError.type].companion}"
-              val raise = tq"${symbolOf[Raise.type].companion}"
+              val raise = tq"${symbolOf[Handle.type].companion}"
               val statefulFunction = tq"${symbolOf[StatefulFunction.type].companion}"
               val functionDescriptor = tq"${symbolOf[FunctionDescriptor]}"
-              val objName = TermName(func.name.toString)
-              val duplicateParams = func.tparams.map { typeDef =>
-                TypeDef(typeDef.mods, TypeName(c.freshName()), typeDef.tparams, typeDef.rhs)
-              }
+              val codec = tq"${symbolOf[Codec.type].asClass.module.info.member(TermName("apply"))}"
               val typeIdents = func.tparams.map(_.name).map(Ident(_))
               val nonInputParams = func.vparamss.map(_.filter(_ != input))
               val nonFlinkInputs = nonInputParams.init :+ nonInputParams.last ++
@@ -79,49 +89,53 @@ object FlinkFunctionImpl {
                 case `input`   => Ident(TermName(msg))
                 case otherwise => Ident(otherwise.name)
               })
+              val AppliedTypeTree(Ident(TypeName(_)), List(returnType)) = func.tpt
+
               def serializerGenerator(wrapper: Symbol): DefDef =
                 q"""
-                  def serializedInput[..${func.tparams}](...$nonFlinkInputs) = {
-                    $wrapper(($msgValDef) => apply[..${typeIdents}](...$withFlinkInputs))
-                  }
-                  """
-              val serializerSpecific = func.mods.annotations.collect {
-                case q"new ProtoInput()" =>
-                  val protoInput =
-                    symbolOf[StatefulFunction.type].asClass.module.info
-                      .member(TermName("protoInput"))
-                  serializerGenerator(protoInput)
-                case q"new CodecInput()" =>
-                  val codecInput =
-                    symbolOf[StatefulFunction.type].asClass.module.info
-                      .member(TermName("byteInput"))
-                  serializerGenerator(codecInput)
-              }
+                 def serializedInput[..${func.tparams}](...$nonFlinkInputs) = {
+                   $wrapper[$higherKindedType, ${msgValDef.tpt}].andThen(($msgValDef) => apply[..${typeIdents}](...$withFlinkInputs)).run
+                 }
+                 """
 
-              if (serializerSpecific.length > 1)
-                c.abort(
-                  c.enclosingPosition,
-                  "One function cannot accept multiple serializers (e.g. Protobuf and Codec inputs cannot be used for the same function)"
+              def mappingSerializer(mapper: Symbol, mappers: List[Tree]): DefDef =
+                q"""
+                 def serializedInput[..${func.tparams}](...$nonFlinkInputs) = {
+                   $mapper[$higherKindedType, ${msgValDef.tpt}, $returnType](..$mappers)(($msgValDef) => apply[..${typeIdents}](...$withFlinkInputs))
+                 }
+                 """
+
+              val serializedInputDef = {
+                val codecMapping =
+                  symbolOf[StatefulFunction.type].asClass.module.info
+                    .member(TermName("codecMapping"))
+                val codecInputK =
+                  symbolOf[StatefulFunction.type].asClass.module.info
+                    .member(TermName("codecInputK"))
+                val mappingSerialized = mappings.map(mapping =>
+                  q"$codecInputK[$higherKindedType, ${mapping.vparams.head.tpt}].map(${mapping}).run"
                 )
-
-              val renameToApply =
-                q"def apply[..${func.tparams}](...${func.vparamss}): ${func.tpt} = ${func.rhs}"
+                if (mappings.isEmpty) serializerGenerator(codecInputK)
+                else mappingSerializer(codecMapping, mappingSerialized)
+              }
 
               val fName = TypeName(c.freshName())
               val stateName = TypeName(c.freshName())
+              val objName = TermName(func.name.toString)
               q"""
               object $objName extends $functionDescriptor {
                 override val namespaceType = ($namespace, $tpe)
                 def send[$fName[_], $stateName](id: String, msg: ${input.tpt})(implicit statefun: $statefulFunction[$fName, $stateName]) =
-                  statefun.sendMsg($namespace, $tpe, id, msg)
-                ..${serializerSpecific}
-                ..${renameToApply}
+                  statefun.sendMsg($namespace, $tpe, id, $codec[${input.tpt}].pack(msg))
+                def apply[..${func.tparams}](...${func.vparamss}): ${func.tpt} = ${func.rhs}
+                ..${serializedInputDef}
               }
               """
           }
           .getOrElse(EmptyTree)
       case _ => EmptyTree
     }
-    q"..$addition"
+    // println(out)
+    out
   }
 }
